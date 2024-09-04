1eb606ea99f03cd26ab90992fa34ae84b6f25ade
IGLIKOWSKI <iglikowskicristiani@gmail.com>
2024-09-04 18:22:13 -0300

    exemplo de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+                //tipos de dados
+                byte idade = 123;
+                short ano = 2021;
+                int cep = 89037800; //cuidado se começar com 0
+                long cpf = 123456789111L; //cuiado se começar com 0
+                float pi = 3.14F;
+                double salario = 1234.56;
     }
 }
