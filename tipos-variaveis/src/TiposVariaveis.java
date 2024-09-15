c9feeea37d9c7b206855c37ba3fdf7967d4046bd
IGLIKOWSKI <iglikowskicristiani@gmail.com>
2024-09-15 15:59:08 -0300

    exercicio: SmartTv

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -4,7 +4,7 @@ public class TiposVariaveis {
                 byte idade = 123;
                 short ano = 2021;
                 int cep = 89037800; //cuidado se começar com 0
-                long cpf = 123456789111L; //cuiado se começar com 0
+                long cpf = 123456789111L; //cuidado se começar com 0
                 float pi = 3.14F;
                 double salario = 1234.56;
     }
