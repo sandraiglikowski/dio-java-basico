87f1b6985fd249278d67ab423a9efadf3b05b537
IGLIKOWSKI <iglikowskicristiani@gmail.com>
2024-09-17 21:46:01 -0300

    add uma pequena interação com o usuario

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,28 +1,41 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Usuario {
     public static void main(String[] args) throws Exception {
 
         SmartTv smartTv = new SmartTv();
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        smartTv.diminuirVolume();
-        smartTv.diminuirVolume();
-        smartTv.diminuirVolume();
-        smartTv.aumentarVolume();
+        System.out.println("Canal " + smartTv.canal);
+        System.out.println("Volume " + smartTv.volume);
 
-        System.out.println("Canal atual: " + smartTv.canal);
-        smartTv.mudarCanal(13);
-        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.print("Escolha um canal: ");
+        smartTv.canal = scanner.nextInt();
+        System.out.println("Canal " + smartTv.canal);
 
-        System.out.println("Volume atual : " + smartTv.volume);
-
-        System.out.println("TV ligada ? " + smartTv.ligada);
-        System.out.println("Canal atual ? " + smartTv.canal);
-        System.out.println("Volume atual ? " + smartTv.volume);
-
-        smartTv.ligar();
-        System.out.println("Novo status -> TV ligada ? " + smartTv.ligada);
-
-        smartTv.desligar();
-        System.out.println("Novo status -> TV ligada ? " + smartTv.ligada);
-
+        /*
+         * smartTv.diminuirVolume();
+         * smartTv.diminuirVolume();
+         * smartTv.diminuirVolume();
+         * smartTv.aumentarVolume();
+         * 
+         * System.out.println("Canal atual: " + smartTv.canal);
+         * smartTv.mudarCanal(13);
+         * System.out.println("Canal atual: " + smartTv.canal);
+         * 
+         * System.out.println("Volume atual : " + smartTv.volume);
+         * 
+         * System.out.println("TV ligada ? " + smartTv.ligada);
+         * System.out.println("Canal atual ? " + smartTv.canal);
+         * System.out.println("Volume atual ? " + smartTv.volume);
+         * 
+         * smartTv.ligar();
+         * System.out.println("Novo status -> TV ligada ? " + smartTv.ligada);
+         * 
+         * smartTv.desligar();
+         * System.out.println("Novo status -> TV ligada ? " + smartTv.ligada);
+         * 
+         */
     }
 }
