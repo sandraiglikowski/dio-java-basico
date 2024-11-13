8bec1eed27c4ab0c39dfecb04f1300889a67977f
IGLIKOWSKI <iglikowskicristiani@gmail.com>
2024-11-13 20:25:21 -0300

    ligando para cada candidato selecionado

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,46 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
         // System.out.println(" ");
         // selecaoCandidatos();
-        imprimirSelecionados();
+        // imprimirSelecionados();
+        String[] candidatos = { "FELIPE", "MARCIA", "JULIA", "PAULO", "DANIELA" };
+        for (String candidato : candidatos) {
+            entrandoEmContato(candidato);
+        }
+    }
+
+    // #2
+    static void entrandoEmContato(String candidato) {
+        int tentativasRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do {
+            atendeu = atender();
+            continuarTentando = !atendeu;// sinal para inverter
+            if (continuarTentando)
+                tentativasRealizadas++;
+            else
+                System.out.println("CONTATO REALIZADO COM SUCESSO!");
+
+            // em algum momento precisarão sofrer alterações
+        } while (continuarTentando && tentativasRealizadas < 3);
+
+        if (atendeu)
+            System.out.println("CONSEGUIMOS CONTATO COM " + candidato + " NA " + tentativasRealizadas + "ª TENTATIVA.");
+        else
+            System.out.println("NÃO CONSEGUIMOS CONTATO COM " + candidato + ".");
+    }
+
+
+    // Método auxiliar #1
+    static boolean atender() {
+        return new Random().nextInt(3) == 1;
+        // valor entre 1 e 3 significa que atendeu
     }
 
     static void imprimirSelecionados() {
